57f88844234e76bc24fcad75e169f6a88d5bb86c
OneBytePython <m.david.emails@gmail.com>
2025-05-03 07:27:59 +0200

    Add files via upload

---
+++ #06 Getting User Input with input().py
@@ -0,0 +1,8 @@
+name = input("What's your name? ")
+print("Nice to meet you,", name + "!")
+
+age = input("How old are you? ")
+print("Wow,", age, "is a great age!")
+
+print("The type of name is:" ,type(name))
+print("The type of age is:" ,type(age))
