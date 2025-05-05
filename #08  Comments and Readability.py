9c28b372c376e5c960e04130ca510d18c2e313d8
OneBytePython <m.david.emails@gmail.com>
2025-05-05 21:18:54 +0200

    Add files via upload

---
+++ #08  Comments and Readability.py
@@ -0,0 +1,19 @@
+width = 5
+height = 6
+
+# This is a calculation -> wrong
+area = width * height
+
+# Calculate the area for.... (purpose) -> good
+area = width * height
+
+# x variable name not denote what is it -> wrong
+x = "David"
+
+# uder_name variable name tells what is stored in it -> good
+uder_name = "David"
+
+"""
+This section handles user authentication.
+Make sure the token is valid.
+""" 
